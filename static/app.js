254904efeae436decf5a6611287208ddb375a366
Jake202020 <slepjake@gmail.com>
2020-07-22 11:30:27 -0400

    timer for game

--- static/app.js
+++ static/app.js
@@ -1,10 +1,29 @@
-let score = 0
+let score = 0;
+let words = new Set();
+let time = 60;
+
+let timer = setInterval(function () {
+    $("#timer").text(`${time} seconds`)
+    time -= 1;
+    if (time === -1) {
+        clearInterval(timer)
+        $('#guess').attr("disabled", "disabled")
+    }
+
+}, 1000)
 
 $('#guess-form').on('submit', async function (evt) {
     evt.preventDefault();
     const $word = $("#guess")
 
     let word = $word.val();
+    if (!word) return;
+
+    if (words.has(word)) {
+        $("#message").text(`${word} already guessed`)
+        $word.val("")
+        return
+    }
 
     const resp = await axios.get("/check-word", { params: { word: word } });
     msg = resp.data.result
@@ -18,6 +37,8 @@ $('#guess-form').on('submit', async function (evt) {
     }
 
     else {
+        words.add(word)
+
         $("#message").text(`${word} is okay`)
 
         $("#score").text(gameScore(word))
